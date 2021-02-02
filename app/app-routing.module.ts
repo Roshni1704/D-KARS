4bf53b5fad4357144fddf314b7690786b53e5803
Roshni1704 <66768416+Roshni1704@users.noreply.github.com>
2021-02-03 01:51:01 +0530

    Add files via upload
    login, registration working (ayu's) -used for demo

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { CardDetailsComponent } from './card-details/card-details.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { WalletComponent } from './wallet/wallet.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookingComponent } from './booking/booking.component';
 
 const routes: Routes = [
   {path:'customer/add',component:RegisterComponent},
   {path:'customer/login',component:LoginComponent},
+  {path:'customer/dashboard',component:DashboardComponent},
   {path:'customer/wallet/:id',component:WalletComponent},
-  {path:'customer/card/:id',component:CardDetailsComponent}
+  {path:'customer/card/:id',component:CardDetailsComponent},
+  {path:'parking/booking/:id',component:BookingComponent}
 ];
 
 @NgModule({
@@ -18,4 +22,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [RegisterComponent,LoginComponent,WalletComponent,CardDetailsComponent];
+//export const routingComponents = [RegisterComponent,LoginComponent,WalletComponent,CardDetailsComponent,DashboardComponent];
