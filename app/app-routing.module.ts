f57697f897fffce47c7931c47f01134c8ce324c7
Roshni1704 <66768416+Roshni1704@users.noreply.github.com>
2021-02-04 13:26:19 +0530

    Add files via upload
    dashboard added w/o integration

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -6,14 +6,18 @@ import { RegisterComponent } from './register/register.component';
 import { WalletComponent } from './wallet/wallet.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookingComponent } from './booking/booking.component';
+import { CheckinComponent } from './checkin/checkin.component';
+import { CheckoutComponent } from "./checkout/checkout.component";
 
 const routes: Routes = [
-  {path:'customer/add',component:RegisterComponent},
+  {path:'customer/register',component:RegisterComponent},
   {path:'customer/login',component:LoginComponent},
-  {path:'customer/dashboard',component:DashboardComponent},
+  {path:'dashboard',component:DashboardComponent},
   {path:'customer/wallet/:id',component:WalletComponent},
   {path:'customer/card/:id',component:CardDetailsComponent},
-  {path:'parking/booking/:id',component:BookingComponent}
+  {path:'parking/booking/:id',component:BookingComponent},
+  {path:'customer/checkin/:id', component:CheckinComponent},
+  {path:'customer/checkout/:id', component:CheckoutComponent}
 ];
 
 @NgModule({
@@ -21,3 +25,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//export const routingComponents = [RegisterComponent,LoginComponent,WalletComponent,CardDetailsComponent,DashboardComponent];
