356aa4aa4fa556d8b222978cc7be09d62713fe37
Roshni1704 <66768416+Roshni1704@users.noreply.github.com>
2021-02-07 16:34:32 +0530

    Add files via upload
    login, register, nav, home, dashboard final

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CardDetailsComponent } from './card-details/card-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WalletComponent } from './wallet/wallet.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookingComponent } from './booking/booking.component';
+import { HomeComponent } from './home/home.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DisplayParkingLotsComponent } from './display-parking-lots/display-parking-lots.component';
+import { ParkingComponent } from './parking/parking.component';
+import { CheckinComponent } from './checkin/checkin.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
+
+const routes: Routes = [
+  // {path:'',redirectTo:'HomeComponent',pathMatch:'full'},
+  {path: '', component:HomeComponent},
+  {path:'customer/add',component:RegisterComponent},
+  {path:'customer/login',component:LoginComponent},
+  {path:'customer/dashboard',component:DashboardComponent},
+  {path:'customer/wallet/:id',component:WalletComponent},
+  {path:'customer/card/:id',component:CardDetailsComponent},
+  {path:'booking/:id',component:BookingComponent},
+  {path:'parking',component:ParkingComponent},
+  {path:'parking/details/:location',component:DisplayParkingLotsComponent},
+  {path:'checkout/:id',component:CheckoutComponent},
+  {path:'checkin/:id',component:CheckinComponent},
+  {path:'home',component:HomeComponent},
+  {path:'navigation',component:NavigationBarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
